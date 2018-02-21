0e3d2f7805fe089f4cd9aba152b5ba922695540a
MistyWorld <kalicem76@hotmail.fr>
2018-02-21 01:51:14 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = "http://gallion.ddns.net:8117";
+
+var poolHost = "gallion.ddns.net";
+
+var irc = "irc.freenode.net/#poolhost";
+
+var email = "contact@gallion.ddns.net";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://gallion.ddns.net/blockchain/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://gallion.ddns.net/blockchain/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/motherboard-dark-theme.css";
